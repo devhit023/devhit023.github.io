65ab7439d2795f99d8673f29f1ae4cc41a9a97a0
jhoon <jhoon6886@gmail.com>
2024-04-21 18:12:46 +0900

    commit

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -3,8 +3,50 @@ $(document).ready(function () {
     function () {
       if ($(this).parent().attr("change") == "true") {
         $(this).parent().attr("change", "false");
+        var classes = $(this).children("span").attr("class").split(" ");
+        for (var i = 0; i < classes.length; i++) {
+          if (classes[i].indexOf("-semibold") !== -1) {
+            var newClass = classes[i].replace("-semibold", "-regular");
+            $(this).children("span").removeClass(classes[i]);
+            $(this).children("span").addClass(newClass);
+          }
+        }
       } else {
         $(this).parent().attr("change", "true");
+        var classes = $(this).children("span").attr("class").split(" ");
+        for (var i = 0; i < classes.length; i++) {
+          if (classes[i].indexOf("-regular") !== -1) {
+            var newClass = classes[i].replace("-regular", "-semibold");
+            $(this).children("span").removeClass(classes[i]);
+            $(this).children("span").addClass(newClass);
+          }
+        }
+      }
+    }
+  );
+  $(".dropdown_wrap:not([type='calendar']) > .dropdown_button").hover(
+    function () {
+      if ($(this).parent().attr("change") == "false") {
+        var classes = $(this).children("span").attr("class").split(" ");
+        for (var i = 0; i < classes.length; i++) {
+          if (classes[i].indexOf("-regular") !== -1) {
+            var newClass = classes[i].replace("-regular", "-semibold");
+            $(this).children("span").removeClass(classes[i]);
+            $(this).children("span").addClass(newClass);
+          }
+        }
+      }
+    },
+    function () {
+      if ($(this).parent().attr("change") == "false") {
+        var classes = $(this).children("span").attr("class").split(" ");
+        for (var i = 0; i < classes.length; i++) {
+          if (classes[i].indexOf("-semibold") !== -1) {
+            var newClass = classes[i].replace("-semibold", "-regular");
+            $(this).children("span").removeClass(classes[i]);
+            $(this).children("span").addClass(newClass);
+          }
+        }
       }
     }
   );
@@ -25,4 +67,15 @@ $(document).ready(function () {
   $(".dropdown_wrap[type='calendar'] .dropdown-menu_item").click(function () {
     $(this).parent().parent().attr("change", "false");
   });
+
+  // $(
+  //   ".main_box .table_search_container .item-list .dropdown_wrap .dropdown-menu_item"
+  // ).click(function () {
+  //   $(this)
+  //     .parent()
+  //     .parent()
+  //     .children(".dropdown_button")
+  //     .children("span")
+  //     .hasClass("heading-01-regular");
+  // });
 });
